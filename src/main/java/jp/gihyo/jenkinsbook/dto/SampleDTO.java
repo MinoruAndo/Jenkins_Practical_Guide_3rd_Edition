9a91cbb941e08be1808af30b032d8a151dd60e7d
Minoru Ando <mix_and@yahoo.co.jp>
2023-12-05 22:36:57 +0900

    Update SampleDTO.java

--- src/main/java/jp/gihyo/jenkinsbook/dto/SampleDTO.java
+++ src/main/java/jp/gihyo/jenkinsbook/dto/SampleDTO.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 /**
  * DTO class for SampleServlet.
  */
-public class SampleDTO {
+public class SampleDTO implements Serializable　{
     /**
      * First name of the user.
      */
